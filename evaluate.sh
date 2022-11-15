2d21d1bd0d95e0f71110b053c889e31d7b8b0d24
Yikai Guo <100450691@alumnos.uc3m.es>
2022-11-15 10:39:04 +0100

    Creating evaluation script

---
+++ evaluate.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+ENERGY_PARAMETERS="energy-cores,energy-gpu,energy-pkg,energy-ram"
+
+echo "Evaluating the 3 functions both for SOA & AOS"
+
+echo "------------ HISTOGRAM (AOS) ------------ "
+
+echo "Performance:"
+perf stat ./build/img-aos ./in ./out histo
+
+echo "Energy:"
+perf stat -a -e $ENERGY_PARAMETERS ./build/img-aos ./in ./out histo
+
+echo "------------ GRAYSCALE (AOS) ------------ "
+
+echo "Performance:"
+perf stat ./build/img-aos ./in ./out mono
+
+echo "Energy:"
+perf stat -a -e $ENERGY_PARAMETERS ./build/img-aos ./in ./out mono
+
+echo "------------ GAUSS (AOS) ------------ "
+
+echo "Performance:"
+perf stat ./build/img-aos ./in ./out gauss
+
+echo "Energy:"
+perf stat -a -e $ENERGY_PARAMETERS ./build/img-aos ./in ./out gauss
+
+echo "------------ HISTOGRAM (SOA) ------------ "
+
+echo "Performance:"
+perf stat ./build/img-soa ./in ./out histo
+
+echo "Energy:"
+perf stat -a -e $ENERGY_PARAMETERS ./build/img-soa ./in ./out histo
+
+echo "------------ GRAYSCALE (SOA) ------------ "
+
+echo "Performance:"
+perf stat ./build/img-soa ./in ./out mono
+
+echo "Energy:"
+perf stat -a -e $ENERGY_PARAMETERS ./build/img-soa ./in ./out mono
+
+echo "------------ GAUSS (SOA) ------------ "
+
+echo "Performance:"
+perf stat ./build/img-soa ./in ./out gauss
+
+echo "Energy:"
+perf stat -a -e $ENERGY_PARAMETERS ./build/img-soa ./in ./out gauss
+
+echo "Finished evaluating"
