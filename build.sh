f5ebfd4c220f517cfb0ecb5996b3528e8bcd3458
MarkGuo2002 <74621162+MarkGuo2002@users.noreply.github.com>
2022-11-21 13:47:32 +0100

    Creating build script file

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export LD_LIBRARY_PATH=/opt/gcc-12.1.0/lib64
+cmake -S . -B release -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/opt/gcc-12.1.0/bin/c++
+cmake --build release
+cd ./release
+make
